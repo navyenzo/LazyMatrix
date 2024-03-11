85b82e89c874d61cbc6bb1659d672089cfb46374
Vincenzo Barbato <navyenzo@gmail.com>
2024-03-11 09:16:04 -0700

    removed explicit search of MSYS2_ROOT directory as i made vcpkg part of msys2

--- toolchains/toolchain.cmake
+++ toolchains/toolchain.cmake
@@ -1,17 +1,10 @@
 # Ensure VCPKG_ROOT is set as a fallback
 if(NOT DEFINED ENV{VCPKG_ROOT})
-  message(WARNING "VCPKG_ROOT environment variable is not set. Falling back to other options if available.")
+  message(WARNING "VCPKG_ROOT environment variable is not set. Falling back to default paths if available.")
 endif()
 
 # Set VCPKG_ROOT from the environment variable
 set(VCPKG_ROOT $ENV{VCPKG_ROOT} CACHE PATH "Path to vcpkg installation")
-
-# New: Check for MSYS2_ROOT environment variable
-if(NOT DEFINED ENV{MSYS2_ROOT})
-  message(WARNING "MSYS2_ROOT environment variable is not set. Falling back to vcpkg if necessary.")
-else()
-  set(MSYS2_ROOT $ENV{MSYS2_ROOT} CACHE PATH "Path to MSYS2 installation")
-endif()
 
 # Default triplet values for vcpkg
 set(VCPKG_DEFAULT_TRIPLET_LINUX "x64-linux")
@@ -24,12 +17,6 @@ if(WIN32)
   if(MSVC)
     message(STATUS "Configuring for MSVC")
     set(VCPKG_TARGET_TRIPLET ${VCPKG_DEFAULT_TRIPLET_WINDOWS} CACHE STRING "Vcpkg target triplet for MSVC" FORCE)
-  # Prioritize MSYS2 if it's available
-  elseif(DEFINED ENV{MSYS2_ROOT} AND EXISTS "$ENV{MSYS2_ROOT}")
-    message(STATUS "MSYS2 detected. Configuring to use MSYS2 for package management.")
-    # Set CMAKE_PREFIX_PATH to use MSYS2 paths
-    set(CMAKE_PREFIX_PATH "$ENV{MSYS2_ROOT}/mingw64" ${CMAKE_PREFIX_PATH})
-    # Note: Additional configuration specific to MSYS2 can be added here
   else()
     message(STATUS "Configuring for MinGW or another Windows compiler")
     set(VCPKG_TARGET_TRIPLET ${VCPKG_DEFAULT_TRIPLET_WINDOWS_MINGW} CACHE STRING "Vcpkg target triplet for MinGW or another Windows compiler" FORCE)
@@ -42,12 +29,9 @@ else()
   message(FATAL_ERROR "Unsupported platform.")
 endif()
 
-# Set the CMAKE_TOOLCHAIN_FILE to use vcpkg if MSYS2 is not being explicitly used
-if(NOT DEFINED ENV{MSYS2_ROOT} OR NOT EXISTS "$ENV{MSYS2_ROOT}")
-  set(CMAKE_TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "Vcpkg toolchain file")
-endif()
+# Set the CMAKE_TOOLCHAIN_FILE to use vcpkg
+set(CMAKE_TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "Vcpkg toolchain file")
 
 # Debugging messages to verify configurations
 message(STATUS "Using VCPKG_ROOT: ${VCPKG_ROOT}")
-message(STATUS "Using MSYS2_ROOT: ${MSYS2_ROOT}")
 message(STATUS "Vcpkg target triplet: ${VCPKG_TARGET_TRIPLET}")
